1d413fc89640ef4828829b647e0aa6832c61261a
Toma547 <toma1228.ashbf@gmail.com>
2025-12-26 16:52:50 +0900

    First Commit

---
+++ crud_test/src/test01/cytech01.java
@@ -0,0 +1,19 @@
+package test01;
+
+public class cytech01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//変数の宣言
+		int num = 100;
+		String text = "あいうえお";
+		boolean flag = true;
+		
+		//出力
+		System.out.println(num);
+		System.out.println(text);
+		System.out.println(flag);
+	}
+
+}
